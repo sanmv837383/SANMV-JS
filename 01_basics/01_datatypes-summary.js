bfd3ec49a40bb488cb8d93155fa7147ec65b19d4
sanmv837383 <164620701+sanmv837383@users.noreply.github.com>
2024-03-23 16:53:22 +0000

    datatype summary

---
+++ 01_basics/01_datatypes-summary.js
@@ -0,0 +1,33 @@
+/* Primitive 
+call by value original value is not given the value is copied and given and the changes made are done in the copy place
+7 types : string , number, boolean, null, undefined, symbol, bigint
+
+const score = 100
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId); --false because the sybols are unique even everything in the symbol is same
+
+const bigNumber = 3456567n
+
+
+javascript is statically typed or dynamically typed statically typed or dynamically typed???
+Reference (non primitive)
+array, objects, functions
+
+const heroes = ["shaktiman","avengers"];     -- reference type or non returnable type
+let myObj = {                                -- objects are always in the curly braces 
+    name: "sanjana",
+    age : 22, 
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+return typr os object function
+
+mention and find out the return type of everything mentioned here
+
+console.log(typeof anotherId);
+*/
