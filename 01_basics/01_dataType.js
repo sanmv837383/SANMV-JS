9d0272c82adfd51ef49d341365dd88c91121e4cb
sanmv837383 <164620701+sanmv837383@users.noreply.github.com>
2024-03-23 10:45:26 +0000

    datatypes

--- 01_basics/01_dataType.js
+++ 01_basics/01_dataType.js
@@ -1,6 +1,23 @@
 "use strict"; // treat  all JS code as newer version
-// alert (3+3) // we are suing nodejs, not browser
+// alert (3+3) // we are using nodejs, not browser
 
 
 console.log(3+3)
 console.log("sanjana")
+
+let name ="sanjana"
+let age = 18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean=> true/false
+// null => stanndalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof "sanjana");
+
+// typescript of null is object
