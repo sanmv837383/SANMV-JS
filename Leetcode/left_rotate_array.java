ec4e66efaec653ff715875fa4c9930b03549f31c
sanmv837383 <164620701+sanmv837383@users.noreply.github.com>
2024-04-09 16:23:51 +0000

    array

---
+++ Leetcode/left_rotate_array.java
@@ -0,0 +1,5 @@
+// left rotate array by n number of places
+[1,2,3,4,5,6,7] there are 7 positions in the array if n = 7 then after the rotation the element comes 
+back to the same position . Hence 7+1 = next position from the existing position
+7+2 = after the next position from the current position. silmilarly the program works
+therefore d = d % n
